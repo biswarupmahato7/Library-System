32af8dbb00627f84364619158a43048c9b03f8c0
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2025-01-27 19:46:56 +0530

    Improve home page for mobile screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,16 +15,17 @@ const Home = () => {
 
       {/* Welcome Section */}
       <div
-        className="mx-auto px-4 flex flex-col justify-center py-12 text-center bg-cover bg-center bg-opacity-30 -z-10"
+        className="mx-auto px-4 flex flex-col justify-center items-center py-12 text-center bg-cover bg-center bg-opacity-30 -z-10"
         style={{
-          backgroundImage: "url('https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-          height: "60vh", 
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          height: '60vh',
         }}
       >
-        <h2 className="text-8xl font-bold text-black mb-4 shadow-lg">
+        <h2 className="text-4xl font-bold text-black mb-4 shadow-lg sm:text-5xl md:text-6xl lg:text-8xl break-words">
           Welcome to BookStore
         </h2>
-        <p className="text-4xl text-black">
+        <p className="text-lg text-black sm:text-xl md:text-2xl lg:text-4xl">
           Read your favorite books and explore new adventures.
         </p>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,70 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import About from './components/About.jsx'
-import { BooksData } from './utils/mockData.js'
-import Error from './components/Error.jsx'
-import Home from './components/Home.jsx'
-import Search from './components/Search.jsx'
-import AllBooks from './components/AllBooks.jsx'
-import AddBooks from './components/AddBooks.jsx'
-import BookDetails from './components/BookDetails.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import About from './components/About.jsx'
+import { BooksData } from "./utils/mockData.js";
+import Error from "./components/Error.jsx";
+import Home from "./components/Home.jsx";
+import Search from "./components/Search.jsx";
+import AllBooks from "./components/AllBooks.jsx";
+import AddBooks from "./components/AddBooks.jsx";
+import BookDetails from "./components/BookDetails.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import {lazy, Suspense} from 'react'
+
+const About = lazy(() => import("./components/About.jsx"));
 
 const appRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path: '/',
+        path: "/",
         element: (
           <>
-          <Home/>
-          <AllBooks booksData={BooksData}/>
+            <Home />
+            <AllBooks booksData={BooksData} />
           </>
-        ) 
+        ),
       },
       {
-        path: '/about',
-        element: <About/>
-      },
-    
-      {
-        path: '/all-books',
+        path: "/about",
         element: (
-          <>
-          <Search/> 
-           <AllBooks booksData={BooksData}/>
-          </>
-        )
-     
+          <Suspense fallback ={ <div>Loading....</div>}>
+            <About />
+          </Suspense>
+        ),
       },
-      {
-        path: '/add-book',
-        element: <AddBooks/>
-      },
-      {
-        path: '/book-details/:id',
-        element: <BookDetails/>
-      }
-      
-    ],
-    errorElement: <Error/>
-  },
 
- 
-])
-createRoot(document.getElementById('root')).render(
+      {
+        path: "/all-books",
+        element: (
+          <>
+            <Search />
+            <AllBooks booksData={BooksData} />
+          </>
+        ),
+      },
+      {
+        path: "/add-book",
+        element: <AddBooks />,
+      },
+      {
+        path: "/book-details/:id",
+        element: <BookDetails />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-
-      <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
